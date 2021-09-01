15333751c1258e28a2e87e17939a43e5f7d2dbb5
Dylan Turnbull <dturbull@seismic.com>
2021-09-01 10:45:24 -0500

    update download link

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'python-opnsense',
   packages = ['opnsense_api'],
-  version = '0.1',
+  version = '0.1.1',
   license='apache-2.0',
   description = 'A python library that interacts with an Opnsense API',
   author = 'Dylan Turnbull',
   author_email = 'dylanturn@gmail.com',
   url = 'https://github.com/turnbros/python-opnsense',
-  download_url = 'https://github.com/turnbros/python-opnsense/v_01.tar.gz',
+  download_url = 'https://github.com/turnbros/python-opnsense/archive/refs/tags/0.1.1.tar.gz',
   keywords = ['turnbros', 'opnsense'],
   install_requires=[],
   classifiers=[
