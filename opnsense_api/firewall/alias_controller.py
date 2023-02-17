d055c72b690fac73d5ea5b80a7f0bb5f3ac5868d
Ian Paul Falatik <ianpaul.falatik@arcor.de>
2023-02-17 15:09:41 +0100

    fix recursion error

--- opnsense_api/firewall/alias_controller.py
+++ opnsense_api/firewall/alias_controller.py
@@ -11,7 +11,7 @@ class Alias(object):
     self.device = device
 
   def list(self) -> list:
-    return self.list()
+    return self.list_aliases()
 
   @deprecated(deprecated_in="1.0.5", removed_in="1.1.0", details="Use list instead")
   def list_aliases(self) -> list:
