973d3d6fff2cf3d13a11db6947e1bd88db7ade02
Ian Falatik <ianpaul.falatik@arcor.de>
2023-02-20 16:49:06 +0100

    fix indentation

--- opnsense_api/interfaces/loopback_controller.py
+++ opnsense_api/interfaces/loopback_controller.py
@@ -7,24 +7,24 @@ log = logging.getLogger(__name__)
 
 @dataclass
 class LoopbackInterface(OPNsenseItem):
-  device_id: int = field(metadata={"json_name": "deviceId"})
-  description: str
+    device_id: int = field(metadata={"json_name": "deviceId"})
+    description: str
 
 
 class LoopbackController(OPNsenseItemController[LoopbackInterface]):
 
-  def __init__(self, device):
-    super().__init__(device, "interface", "loopback_settings")
+    def __init__(self, device):
+        super().__init__(device, "interface", "loopback_settings")
 
-  def _parse_api_response(self, api_response: dict) -> LoopbackInterface:
-    return LoopbackInterface(
-      uuid=api_response["uuid"],
-      device_id=api_response["deviceId"],
-      description=api_response["description"]
-    )
+    def _parse_api_response(self, api_response: dict) -> LoopbackInterface:
+        return LoopbackInterface(
+        uuid=api_response["uuid"],
+        device_id=api_response["deviceId"],
+        description=api_response["description"]
+        )
 
-  def add(self, interface: LoopbackInterface) -> LoopbackInterface:
-    pass
+    def add(self, interface: LoopbackInterface) -> LoopbackInterface:
+        pass
 
-  def set(self, interface: LoopbackInterface) -> LoopbackInterface:
-    pass
+    def set(self, interface: LoopbackInterface) -> LoopbackInterface:
+        pass
--- opnsense_api/interfaces/vxlan_controller.py
+++ opnsense_api/interfaces/vxlan_controller.py
@@ -7,20 +7,20 @@ log = logging.getLogger(__name__)
 
 @dataclass
 class VXLANInterface(OPNsenseItem):
-  description: str
-  enabled: bool
+    description: str
+    enabled: bool
 
 
 class VXLANController(OPNsenseItemController[VXLANInterface]):
 
-  def __init__(self, device):
-    super().__init__(device, "interface", "vxlan_settings")
+    def __init__(self, device):
+        super().__init__(device, "interface", "vxlan_settings")
 
-  def _parse_api_response(self, api_response) -> VXLANInterface:
-    pass
+    def _parse_api_response(self, api_response) -> VXLANInterface:
+        pass
 
-  def add(self, interface: VXLANInterface) -> VXLANInterface:
-    pass
+    def add(self, interface: VXLANInterface) -> VXLANInterface:
+        pass
 
-  def set(self, interface: VXLANInterface) -> VXLANInterface:
-    pass
+    def set(self, interface: VXLANInterface) -> VXLANInterface:
+        pass
