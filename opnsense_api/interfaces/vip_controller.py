fc73855bb25a5a27cce47f90779c01fe3613aa0d
Dylan Turnbull <dylanturn@gmail.com>
2023-02-19 18:37:35 -0600

    Feature/generics (#3) (#12)
    * Implemented a generic controller for API controllers that primarily consist of CRUD operations
    
    * fixed a couple issues. one duplicate method name and one bad indent

---
+++ opnsense_api/interfaces/vip_controller.py
@@ -0,0 +1,48 @@
+from ..util.item_controller import OPNsenseItemController, OPNsenseItem
+from dataclasses import dataclass
+import logging
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class VIPInterface(OPNsenseItem):
+  description: str
+  interface: str
+  mode: str
+  descr: str
+  subnet: str
+  subnet_bits: str
+  vhid: str
+  advbase: str
+  advskew: str
+  address: str
+  vhid_txt: str
+
+
+class VIPController(OPNsenseItemController[VIPInterface]):
+
+  def __init__(self, device):
+    super().__init__(device, "interface", "vip_settings")
+
+  def _parse_api_response(self, api_response) -> VIPInterface:
+    return VIPInterface(
+      uuid=api_response["uuid"],
+      description=api_response["description"],
+      interface=api_response["interface"],
+      mode=api_response["mode"],
+      descr=api_response["descr"],
+      subnet=api_response["subnet"],
+      subnet_bits=api_response["subnet_bits"],
+      vhid=api_response["vhid"],
+      advbase=api_response["advbase"],
+      advskew=api_response["advskew"],
+      address=api_response["address"],
+      vhid_txt=api_response["vhid_txt"]
+    )
+
+  def add(self, interface: VIPInterface) -> VIPInterface:
+    pass
+
+  def set(self, interface: VIPInterface) -> VIPInterface:
+    pass
