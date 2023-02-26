519deb49e053890dacf3b3c2112abf5f2f1dbf08
Ian Paul Falatik <ianpaul.falatik@arcor.de>
2023-02-26 15:20:20 +0100

    add parse_int

--- opnsense_api/util/parse.py
+++ opnsense_api/util/parse.py
@@ -1,7 +1,16 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import Type
+from typing import Type, Union
+
+
+def parse_int(api_response_part: str) -> Union[int, None]:
+    """
+    Used during parsing of an item
+    :param api_response_part: value of api response that is to be parsed to int
+    :return: integer value of api_response_part contains int, else None
+    """
+    return int(api_response_part) if api_response_part else None
 
 
 def parse_selected_enum(api_response: dict,
