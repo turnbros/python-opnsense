153c0a6e408f76307bd218d9aa57db40f76d0e38
Ian Paul Falatik <ianpaul.falatik@arcor.de>
2023-03-01 23:38:52 +0100

    fix docstrings

--- opnsense_api/util/parse.py
+++ opnsense_api/util/parse.py
@@ -21,7 +21,7 @@ def parse_selected_enum(api_response: dict,
     :param api_response: response from the OPNSense
     :param search_str: What key to search for in the api_response
     :param enum: Enum that return value should belong to
-    :return: enum that match the api_response[search_str] and has 'selected' set to true
+    :return: enum that match the api_response_part and has 'selected' set to true
     """
     for key, val in api_response[search_str].items():
         if bool(val['selected']):
@@ -33,6 +33,6 @@ def parse_selected_keys(api_response_part: dict) -> list[str]:
     """
     Used during parsing of an item.
     :param api_response_part: response part to search in
-    :return: every selected key in api_response[search_str] in a list
+    :return: every selected key in api_response_part in a list
     """
     return [key for key in api_response_part if bool(api_response_part[key]['selected'])]
