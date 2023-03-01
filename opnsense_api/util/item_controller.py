1fe80c6926664fbbff35d4b14b16df586292a002
Ian Paul Falatik <ianpaul.falatik@arcor.de>
2023-03-01 21:37:43 +0100

    remove unnecessary methods

--- opnsense_api/util/item_controller.py
+++ opnsense_api/util/item_controller.py
@@ -95,18 +95,6 @@ class OPNsenseItem(BaseModel, ABC):
                 )
         }
 
-    # TODO: figure out if this is still needed
-    # def to_dict(self):
-    #     opnsense_item = asdict(self)
-    #     opnsense_item_dict = {}
-    #     for field in fields(self):
-    #         opnsense_item_dict[field.metadata.get("json_name", field.name)] = opnsense_item.get(field.name)
-
-    #     return opnsense_item_dict
-
-    # def to_json(self):
-    #     return json.dumps(self.to_dict())
-
 
 class OPNsenseItemController(Generic[TOPNSenseItem], OPNsenseAPIController, ABC):
     # This gets overridden if the controller uses different action verbs
