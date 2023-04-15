885b4d96c8a0957939c53ee5788c4232accee72d
Dylan Turnbull <dylanturn@gmail.com>
2023-04-14 23:47:04 -0500

    Refactor SystemDiagnosticsController to return more detailed memory statistics and add process statistics dataclasses

--- opnsense_api/diagnostics/diagnostics_dataclasses.py
+++ opnsense_api/diagnostics/diagnostics_dataclasses.py
@@ -1,39 +1,11 @@
 from dataclasses import dataclass
-from typing import List
-
 
 @dataclass
-class SystemProcess:
-    c: str  # "0"
-    pid: str  # "11"
-    thr: str  # "100003"
-    username: str  # "root"
-    pri: str  # "155"
-    nice: str  # "ki31"
-    size: str  # "0B"
-    res: str  # "32K"
-    state: str  # "RUN"
-    time: str  # "281.3H"
-    wcpu: str  # "98.08%"
-    command: str  # "[idle{idle: cpu0}]"
-
-
-@dataclass()
-class SystemActivity:
-    system_processes: List[SystemProcess]
-
-
-@dataclass()
-class SystemMemory:
-    pass
-
-
-@dataclass()
 class SystemInterfaces:
     name: str
     description: str
 
 
-@dataclass()
+@dataclass
 class SystemRRDlist:
     pass
--- opnsense_api/diagnostics/system_diagnostics_controller.py
+++ opnsense_api/diagnostics/system_diagnostics_controller.py
@@ -1,5 +1,84 @@
+from dataclasses import dataclass
+from typing import List
+
 from opnsense_api.util.controller import OPNsenseAPIController
-from opnsense_api.diagnostics.diagnostics_dataclasses import SystemMemory, SystemActivity
+
+
+@dataclass
+class ThreadStats:
+    total: int
+    running: int
+    sleeping: int
+    waiting: int
+
+
+@dataclass
+class MemoryStats:
+    pass
+
+
+@dataclass
+class CPUStats:
+    pass
+
+
+@dataclass
+class ProcessStats:
+    c: str  # "0"
+    pid: str  # "11"
+    thr: str  # "100003"
+    username: str  # "root"
+    pri: str  # "155"
+    nice: str  # "ki31"
+    size: str  # "0B"
+    res: str  # "32K"
+    state: str  # "RUN"
+    time: str  # "281.3H"
+    wcpu: str  # "98.08%"
+    command: str  # "[idle{idle: cpu0}]"
+
+
+@dataclass
+class SystemActivity:
+    last_pid: int
+    load_averages: List[int]
+    uptime: int
+    thread_stats: ThreadStats
+    cpu_stats: CPUStats
+    memory_stats: MemoryStats
+    system_processes: List[ProcessStats]
+
+
+@dataclass
+class SystemMemoryAllocationDetails:
+    name: str
+    in_use: int
+    memory_use: int
+    requests: int
+    size: List[int]
+
+
+@dataclass
+class SystemMemoryZoneStatistics:
+    name: str
+    size: int
+    limit: int
+    used: int
+    free: int
+    requests: int
+    fail: int
+    sleep: int
+    xdomain: int
+
+
+@dataclass
+class SystemMemoryDetails:
+
+    memory_allocations: List[SystemMemoryAllocationDetails]
+    total_allocated_memory_used: int
+
+    virtual_memory_zones: List[SystemMemoryZoneStatistics]
+    total_virtual_memory_used: int
 
 
 class SystemDiagnosticsController:
@@ -16,13 +95,13 @@ class SystemDiagnosticsController:
         """
         return self._system_activity_controller.get_activity()
 
-    def get_memory_statistics(self) -> SystemMemory:
+    def get_memory_statistics(self) -> SystemMemoryDetails:
         """
         Returns system memory statistics.
 
-        :return: SystemMemory
+        :return: SystemMemoryDetails
         """
-        return self._system_memory_controller.get_memory()
+        return self._system_memory_controller.get_memory_details()
 
     class _SystemActivity(OPNsenseAPIController):
 
@@ -37,5 +116,5 @@ class SystemDiagnosticsController:
         def __init__(self, device):
             super().__init__(device, "diagnostics", "system")
 
-        def get_memory(self) -> SystemMemory:
+        def get_memory_details(self) -> SystemMemoryDetails:
             return self._api_get("memory")
